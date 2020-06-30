8f70fb44829e12728ff8a362a1d1ef83b465f574
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-06-30 16:26:29 +0200

    Remove no longer needed test

--- Utility.Test/Extension/EnumerableExtensionTest.cs
+++ Utility.Test/Extension/EnumerableExtensionTest.cs
@@ -8,18 +8,6 @@ namespace Messerli.Utility.Test.Extension
     public sealed class EnumerableExtensionTest
     {
         [Fact]
-        public void ForEachExecutesAction()
-        {
-            var elements = System.Linq.Enumerable.Repeat(new ForEachExecutor(), 17);
-
-            // ReSharper disable once PossibleMultipleEnumeration
-            elements.ForEach(element => element.Execute());
-
-            // ReSharper disable once PossibleMultipleEnumeration
-            Assert.True(elements.All(element => element.SideEffect));
-        }
-
-        [Fact]
         public async Task ForEachExecutesActionAsync()
         {
             var elements = System.Linq.Enumerable.Repeat(new ForEachExecutor(), 17);
