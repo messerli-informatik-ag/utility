bd8b5291c7b24ae5c2b4088ed28aaeb97152be9a
Dominic Groth <whitedragon@dragonlabs.ch>
2020-08-28 09:27:25 +0200

    Add unhandled enum variant exception

---
+++ Utility/Exception/UnhandledEnumVariantException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Messerli.Utility.Exception
+{
+    public class UnhandledEnumVariantException : System.Exception
+    {
+        public UnhandledEnumVariantException(Type type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public Type Type { get; }
+
+        public string Value { get; }
+
+        public override string Message => $"Variant {Value} of {Type.Name} is unhandled.";
+    }
+
+    public class UnhandledEnumVariantException<T> : UnhandledEnumVariantException
+    {
+        public UnhandledEnumVariantException(string value)
+            : base(typeof(T), value)
+        {
+        }
+    }
+}
